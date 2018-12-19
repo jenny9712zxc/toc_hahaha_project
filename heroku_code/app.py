cb7f829422819d556bf3be8ced788e03d4fae948
jenny9712zxc <34540847+jenny9712zxc@users.noreply.github.com>
2018-12-19 23:55:27 +0800

    Add files via upload

---
+++ heroku_code/app.py
@@ -0,0 +1,49 @@
+from bottle import route, run, request, abort, static_file
+
+#from fsm import TocMachine
+from fsm import *
+import json
+import os
+
+
+VERIFY_TOKEN = os.environ["VERIFY_TOKEN"]
+PORT=os.environ["PORT"]
+#VERIFY_TOKEN = "123456"
+
+
+@route("/webhook", method="GET")
+def setup_webhook():
+    mode = request.GET.get("hub.mode")
+    token = request.GET.get("hub.verify_token")
+    challenge = request.GET.get("hub.challenge")
+
+    if mode == "subscribe" and token == VERIFY_TOKEN:
+        print("WEBHOOK_VERIFIED")
+        return challenge
+
+    else:
+        abort(403)
+
+
+@route("/webhook", method="POST")
+def webhook_handler():
+    body = request.json
+    print('\nFSM STATE: ' + machine.state)
+    print('REQUEST BODY: ')
+    print(json.dumps(body,indent=4))
+    print('\n\n')
+
+    if body['object'] == "page":
+        event = body['entry'][0]['messaging'][0]
+        machine.advance(event)
+        return 'OK'
+
+
+@route('/show-fsm', methods=['GET'])
+def show_fsm():
+    machine.get_graph().draw('fsm.png', prog='dot', format='png')
+    return static_file('fsm.png', root='./', mimetype='image/png')
+
+
+if __name__ == "__main__":
+    run(host="0.0.0.0", port=PORT, debug=True, reloader=True)
---
+++ heroku_code/utils.py
@@ -0,0 +1,49 @@
+import requests
+import os
+
+GRAPH_URL = "https://graph.facebook.com/v2.6"
+#ACCESS_TOKEN = "EAADAw8AK4VYBACbzDM8AWzVZAmlND0EPByvzD0bdO01eMMHIhjRe2HQVYjZAdyYEA6iz6MqdyjzbEfNhtNpFHYZBpu84EQtxkN7"
+
+ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
+
+def send_text_message(id, text):
+    url = "{0}/me/messages?access_token={1}".format(GRAPH_URL, ACCESS_TOKEN)
+    payload = {
+        "recipient": {"id": id},
+        "message": {"text": text}
+    }
+    response = requests.post(url, json=payload)
+
+    if response.status_code != 200:
+        print("Unable to send message: " + response.text)
+    return response
+
+
+
+def send_image_url(id, img_url):
+   
+    payload = {
+        "recipient": {"id": id},
+        "message": { 
+            "attachments":{
+                "type": "image",
+                "payload": {
+                    "url": img_url,
+                    "is_reusable":true
+                }
+            }
+        }
+    }
+    response = requests.post(url, json=payload)
+
+    if response.status_code != 200:
+        print("Unable to send message: " + response.text)
+    return response
+
+    
+
+
+"""
+def send_button_message(id, text, buttons):
+    pass
+"""
